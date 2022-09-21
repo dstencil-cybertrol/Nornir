78c258d4dfe3826b4634364c9dde7d53228ba396
dstencil <35854396+dstencil@users.noreply.github.com>
2022-09-21 19:22:49 +0000

    Create get_interfaces_ip.py

---
+++ get_interfaces_ip.py
@@ -0,0 +1,29 @@
+from distutils.command.config import config
+import pathlib
+from nornir import InitNornir
+from nornir_utils.plugins.tasks.networking import tcp_ping
+from nornir_utils.plugins.tasks import networking
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.files import write_file
+from nornir_napalm.plugins.tasks import napalm_get
+from datetime import date
+import pathlib
+
+def get_interfaces_ip(task):
+    config_dir = "config-archive"
+    device_dir= config_dir + "/" + task.host.name
+    pathlib.Path(config_dir).mkdir(exist_ok=True)
+    pathlib.Path(device_dir).mkdir(exist_ok=True)
+    r = task.run(napalm_get, getters=["get_interfaces_ip"])
+    task.run(
+        task=write_file,
+        content=r.result["get_interfaces_ip"],
+        filename=f"{str(device_dir)}/facts_{str(date.today())}.txt"
+    )
+
+nr = InitNornir(config_file="/root/nornir/inventory/config.yaml")
+result = nr.run(
+    name="GATHERING DEVICE IP Interfaces", task=get_interfaces_ip
+)
+
+print_result(result)
